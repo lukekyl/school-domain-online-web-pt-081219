ccba37750eb2527bc2c4d2e66c7d60db65e5150b
Kyle Luke <th.oldschool@gmail.com>
2019-08-28 04:10:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_reader :roster 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(num)
+    roster[num]
+  end
+  
+  def sort
+    new_array = {}
+    @roster.each.sort{ |grade|
+      
+      new_array[grade] << @roster[grade]
+    }
+    
+  end
+  
+end
